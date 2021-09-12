964f57f930f9e4042a0c01c0d39d499ab88e2c86
KingCalledSnow <89491627+KingCalledSnow@users.noreply.github.com>
2021-09-12 16:47:37 -0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ namespace Bela
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] vs)
         {
             string[] input = Console.ReadLine().ToUpper().Split(" ");
 
